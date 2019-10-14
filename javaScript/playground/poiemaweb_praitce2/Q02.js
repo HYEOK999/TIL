53f5a4cd303b3c8b6fccd94ce03897435e8e5e15
HYEOK999 <iasg2004@naver.com>
2019-10-14 19:21:41 +0900

    JavaScript Study 08 일차
    - 용어
    - 스코프
      - 순수 함수
      - 비순수 함수
      - 스코프의 종류
      - 스코프 체인
      - 렉시컬 스코프
      - 암묵적 전역 변수
      - JS 모듈 미지원
        - 모듈
    
    - 전역 변수의 문제점
      - 전역 변수 사용 억제 방법
    
    - let, const와 블록 레벨 스코프
      - var 키워드로 선언한 변수의 문제점
      - let 키워드와 var 키워드의 차이점
      - const 키워드
      - const 키워드와 객체
      - var : let : const

--- javaScript/playground/poiemaweb_praitce2/Q02.js
+++ javaScript/playground/poiemaweb_praitce2/Q02.js
@@ -18,6 +18,7 @@ function getCount8() {
       }
     }
   }
+
   return count8;
 }
 
---
+++ javaScript/playground/test4.js
@@ -0,0 +1,8 @@
+console.log('실행시작');
+for(let i = 10; i > 0; i--){
+  if(i % 2 == 1){
+    console.log(i);
+  }
+}
+console.log('실행 끝')
+
