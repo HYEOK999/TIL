e60fa8581c3463633f8da59dc08311b68b7cf770
HYEOK999 <iasg2004@naver.com>
2019-10-15 12:59:04 +0900

    JavaScript Study 09 일차 작성중

--- javaScript/playground/test4.js
+++ javaScript/playground/test4.js
@@ -1,8 +1,10 @@
-console.log('실행시작');
-for(let i = 10; i > 0; i--){
-  if(i % 2 == 1){
-    console.log(i);
-  }
-}
-console.log('실행 끝')
+var x = 10;
 
+function add() {
+  var x = 11;
+  console.log(x);
+  return x;
+}
+
+add();
+console.log(x);
