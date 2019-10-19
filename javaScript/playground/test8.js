13548ca5fdb291a7c22323f958709e2a1ba8e68c
HYEOK999 <iasg2004@naver.com>
2019-10-19 17:13:31 +0900

    docs: Add the Alogrithm release

--- javaScript/playground/test8.js
+++ javaScript/playground/test8.js
@@ -1,22 +1,25 @@
-function Sanggibu(name, grade) {
-  this.name = name;
-  this.grade = grade;
+function solution(progresses, speeds) {
+  var answer = [];
+  let booleanValue;
+  let count;
+
+  while(progresses.length){
+      booleanValue = false;
+      count = 0;
+      for(let i = 0; i < progresses.length; i++){
+          progresses[i] += speeds[i];
+      }
+
+      while (progresses.length != 0 && progresses[0] >= 100) {
+          booleanValue = true;
+          count++;
+          progresses[0].shift;
+          speeds[0].shift;
+      }
+      if(booleanValue == true){
+          answer.push(count);
+      }
+  }
+
+  return answer;
 }
-
-let a = new Sanggibu('kim', 80);
-let b = new Sanggibu('lee', 70);
-let c = new Sanggibu('park', 50);
-let d = new Sanggibu('john', 90);
-
-Object.getPrototypeOf(a).getResult = function () {
-  if (this.grade > 70) return '합격';
-  else{
-    return '불합격';
- }
- return 0;
-}
-
-console.log(a.getResult());
-console.log(b.getResult());
-console.log(c.getResult());
-console.log(d.getResult());
