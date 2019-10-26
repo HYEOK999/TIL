f2ece22f6b10b2027c7b8c06a30edc6793fd6c20
HYEOK999 <iasg2004@naver.com>
2019-10-26 23:26:01 +0900

    docs: Start the Alogrithm release

--- javaScript/playground/test21.js
+++ javaScript/playground/test21.js
@@ -1,19 +1,40 @@
-function solution(s) {
-  let answer = 0;
+// function solution(s) {
+//   let answer = '';
 
-  let temp = '';
+//   console.log(s.split('').sort().reverse().join(''));
+//   return answer;
+// }
+
+// solution('ZbcdefgA');
+
+// function solution(strings, n) {
+//   let answer = [];
+
+
+//   return strings.sort((a, b) => (a[n] > b[n] ? 1 : (a[n] < b[n] ? -1 : (a > b ? 1 : -1))));
+
+// }
+
+// console.log(solution(["abzcd", "cdzab", "abzfg", "abzaa", "abzbb", "bbzaa"],2));
+
+function solution(progresses, speeds) {
+  let answer = [];
   let count = 0;
-  for (let i = 0; i < s.length; i++) {
-    temp += s[i];
 
-    while (s.includes(temp)) {
-
+  while (progresses[0]) {
+    count = 0;
+    for (let i = 0; i < progresses.length; i++) {
+      progresses[i] = progresses[i] + speeds[i];
+    }
+    while (progresses[0] >= 100) {
+      count++;
+      progresses.shift();
     }
 
-    count >= ( i + 1 ) ? count : ( i + 1 );
+    answer.push(count);
   }
 
   return answer;
 }
 
-solution("aabbaccc");
+console.log(solution([93, 30, 55], [1, 30, 5]));
