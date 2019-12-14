0ef4c49b372b435629c2725dfbe5482f5efffaaf
HYEOK999 <iasg2004@naver.com>
2019-12-14 15:27:46 +0900

    docs: Start the Alogrithm release

---
+++ javaScript/playground/test28.js
@@ -0,0 +1,29 @@
+
+function transMap(n, num) {
+  let res = 0;
+  let arr = '';
+  for (let i = 0; i < n; i++) {
+    res = num % 2;
+    num = Math.floor((num /= 2));
+
+    if (res) arr += '#';
+    else arr += ' ';
+  }
+  return arr.split('').reverse().join('');
+}
+
+function solution(n, arr1, arr2) {
+  const completedMap = [];
+  let answer = '';
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      answer += transMap(n, arr1[i])[j] === '#' || transMap(n, arr2[i])[j] === '#' ? '#' : ' ';
+    }
+    completedMap.push(answer);
+    answer = '';
+  }
+  return completedMap;
+}
+
+solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]);
