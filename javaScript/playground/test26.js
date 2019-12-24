65a0654a021204d32aff187950aad1a9f9495cee
HYEOK999 <iasg2004@naver.com>
2019-12-24 20:15:18 +0900

    docs : Study the algorithm

--- javaScript/playground/test26.js
+++ javaScript/playground/test26.js
@@ -1,1 +1,19 @@
+function GCD(a, b) {
+  while (b != 0) {
+    const r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
 
+function LCM(a, b) {
+  return (a * b) / GCD(a, b);
+} // 두수를 곱하고 최대공약수를 빼준다.
+
+function solution(n, m) {
+  const answer = [GCD(n, m), LCM(n, m)];
+  return answer;
+}
+
+console.log(solution(12, 3));
