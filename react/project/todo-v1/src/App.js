8921b959bdbc6b6a31b02e28813f0ae463d08c2e
HYEOK999 <iasg2004@naver.com>
2020-01-06 23:52:50 +0900

    feat : Study the Todo-React

---
+++ react/project/todo-v1/src/App.js
@@ -0,0 +1,126 @@
+import React, { createRef, Component } from 'react'
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos : [],
+      navId : 'all'
+    };
+    this.nav = createRef();
+    this.setTodoLists = this.setTodoLists.bind(this);
+  }
+
+  componentDidMount() {
+    let todos = [
+      { id: 1, content: 'HTML', completed: false },
+      { id: 2, content: 'CSS', completed: true },
+      { id: 3, content: 'Javascript', completed: false }
+    ]
+    this.setTodoLists(todos);
+  }
+
+  setTodoLists(todos) {
+    this.setState({
+      todos : [...this.state.todos, ...todos]
+    })
+  }
+
+  generateId() {
+    return Math.max(0, ...this.state.todos.map((todo) => todo.id)) + 1;
+  }
+
+  addTodo(e) {
+    if (e.key === 'Enter') {
+      let todos = { id: this.generateId() , content: e.target.value, completed: false};
+      this.setTodoLists([todos])
+      e.target.value = '';
+    }
+  }
+
+  removeTodo(target, id) {
+    if(target.classList[0] !== 'remove-todo') return
+    this.setState({
+      todos : this.state.todos.filter((todo) => todo.id !== id)
+    });
+  }
+
+  checkTodo(target, id){
+    if(target.classList[0] !== 'custom-checkbox') return;
+    this.setState({
+      todos : this.state.todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+    })
+  }
+
+  toggleTodos(target) {
+    this.setState({
+      todos : this.state.todos.map((todo) => ({ ...todo, completed: target.checked}))
+    })
+  }
+
+  cleanTodos() {
+    this.setState({
+      todos : this.state.todos.filter((todo) => (!todo.completed))
+    })
+  }
+
+  changeNav(target) {
+    if (target.classList.contains('nav')) return;
+    [...this.nav.current.children].forEach((navList) => {
+      navList.classList.toggle('active', navList.id === target.id);
+      this.setState({
+        navId : target.id
+      })
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    const _todos = this.state.todos.filter(({ completed }) => (this.state.navId === 'all' ?  true : this.state.navId === 'active' ? !completed : completed));
+    const completedCount = _todos.filter(({ completed }) => completed).length;
+    const activeCount = _todos.filter(({ completed }) => !completed).length;
+
+    return (
+      <div className="container">
+        <h1 className="title">Todos</h1>
+        <div className="ver">2.0</div>
+        <input className="input-todo" placeholder="What needs to be done?" autoFocus onKeyPress={(e) => this.addTodo(e)} />
+        <ul className="nav" ref={this.nav} onClick={({target}) => this.changeNav(target)}>
+          <li id="all" className="active">All</li>
+          <li id="active">Active</li>
+          <li id="completed">Completed</li>
+        </ul>
+        <ul className="todos">
+          {
+            _todos.map((todo) => (
+              <li id={todo.id} className="todo-item" key={todo.id}>
+                <input
+                  className="custom-checkbox"
+                  type="checkbox"
+                  onChange={({target}) => this.checkTodo(target, todo.id)}
+                  id={`ck-${todo.id}`}
+                  checked={todo.completed}
+                />
+                <label htmlFor={`ck-${todo.id}`}>{todo.content}</label>
+                <i className="remove-todo far fa-times-circle" onClick={({target}) => this.removeTodo(target, todo.id)}></i>
+              </li>
+            ))
+          }
+        </ul>
+        <div className="footer">
+          <div className="complete-all">
+            <input className="custom-checkbox" type="checkbox" id="ck-complete-all" onClick={({target}) => this.toggleTodos(target)}/>
+            <label htmlFor="ck-complete-all">Mark all as complete</label>
+          </div>
+          <div className="clear-completed">
+            <button className="btn" onClick={() => this.cleanTodos()}>Clear completed (<span className="completed-todos">{completedCount}</span>)</button>
+            <strong className="active-todos">{activeCount}</strong> items left
+          </div>
+        </div>
+    </div>
+    );
+  }
+}
+
+export default App;
