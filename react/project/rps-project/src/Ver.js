66637c38154b303c7786ec310ca4473ebe5057f6
HYEOK999 <iasg2004@naver.com>
2020-01-13 21:23:13 +0900

    feat : Add the rps-project Ver1

---
+++ react/project/rps-project/src/Ver.js
@@ -0,0 +1,69 @@
+import React,
+{
+  useState,
+  useEffect,
+  useRef
+} from 'react';
+// ver. 1
+function Game() {
+  console.log('1. load'); //
+  const [pick, setPick] = useState('');
+  const [myPick, setMyPick] = useState('');
+  const [winner, setWinner] = useState('');
+  const selRef = useRef(null); // ver. 2
+  const runGame = val => {
+    const items = ['가위', '바위', '보'];
+    const ranIdx = Math.floor(Math.random() * items.length);
+    setPick(items[ranIdx]);
+    setMyPick(val);
+  };
+  useEffect(() => {
+    console.log('3. useEffect1')
+    let _winner;
+    if (pick === '가위') {
+      if (myPick === '가위') _winner = 'none';
+      if (myPick === '바위') _winner = 'you';
+      if (myPick === '보') _winner = 'com';
+    } else if (pick === '바위') {
+      if (myPick === '가위') _winner = 'com';
+      if (myPick === '바위') _winner = 'none';
+      if (myPick === '보') _winner = 'you';
+    } else {
+      if (myPick === '가위') _winner = 'you';
+      if (myPick === '바위') _winner = 'com';
+      if (myPick === '보') _winner = 'none';
+    }
+    setWinner(_winner);
+  }, [pick, myPick]);
+  useEffect(() => {
+    console.log('4. useEffect2')
+    console.log('winner is ' + winner);
+  }, [winner]);
+  // ver.1
+  // return (
+  //   <>
+  //     <button onClick={() => runGame('가위')}>가위</button>
+  //     <button onClick={() => runGame('바위')}>바위</button>
+  //     <button onClick={() => runGame('보')}>보</button>
+  //   </>
+  // );
+  const handleSelChange = () => {
+    const val = selRef.current.value;
+    runGame(val);
+    console.log(val);
+  }
+  // 1 2 3 4 1 2
+  // ver. 2
+  return (
+    <>
+      {winner}
+      {(() => console.log('2. render'))()} //
+      <select ref={selRef} onChange={handleSelChange}>
+        <option value="가위">가위</option>
+        <option value="바위">바위</option>
+        <option value="보">보</option>
+      </select>
+    </>
+  );
+}
+export default Game;
