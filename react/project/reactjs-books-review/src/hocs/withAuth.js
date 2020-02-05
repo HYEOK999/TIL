bd3fd1ee4a567f47b6c2b2ce6611251ed78b9172
HYEOK999 <iasg2004@naver.com>
2020-02-05 23:42:36 +0900

    feat : Add the hocs

---
+++ react/project/reactjs-books-review/src/hocs/withAuth.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+function withAuth(Component, loggedin = true) {
+  function WrappedComponent(props) {
+    const token = useSelector(state => state.token);
+
+    if (loggedin) {
+      if (token === null) {
+        return <Redirect to="/signin" />;
+      }
+      return <Component {...props} token={token} />;
+    } else {
+      if (token !== null) {
+        return <Redirect to="/" />;
+      }
+      return <Component {...props} />;
+    }
+  }
+
+  WrappedComponent.displayName = `withAuth(${Component.name})`;
+
+  return WrappedComponent;
+}
+
+export default withAuth;
