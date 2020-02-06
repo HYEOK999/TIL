962518e70760098c0dbcc0caeae33d9435befb04
HYEOK999 <iasg2004@naver.com>
2020-02-06 23:23:29 +0900

    feat : Add the hocs

--- react/project/reactjs-books-review/src/hocs/withAuth.js
+++ react/project/reactjs-books-review/src/hocs/withAuth.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 
 function withAuth(Component, loggedin = true) {
   function WrappedComponent(props) {
-    const token = useSelector(state => state.token);
+    const token = useSelector(state => state.auth.token);
 
     if (loggedin) {
       if (token === null) {
@@ -12,9 +12,9 @@ function withAuth(Component, loggedin = true) {
       }
       return <Component {...props} token={token} />;
     } else {
-      if (token !== null) {
-        return <Redirect to="/" />;
-      }
+      // if (token !== null) {
+      //   return <Redirect to="/" />;
+      // }
       return <Component {...props} />;
     }
   }
