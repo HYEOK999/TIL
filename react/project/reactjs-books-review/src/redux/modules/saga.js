85c9ca3f06e1a84b5e872dcf90b2e407bfd7e6c6
HYEOK999 <iasg2004@naver.com>
2020-02-07 23:40:46 +0900

    feat : Add the sage

--- react/project/reactjs-books-review/src/redux/modules/saga.js
+++ react/project/reactjs-books-review/src/redux/modules/saga.js
@@ -3,7 +3,8 @@
 
 import { all } from 'redux-saga/effects';
 import { booksSaga } from './books';
+import { authSaga } from './auth';
 
 export default function* rootSaga() {
-  yield all([booksSaga()]);
+  yield all([booksSaga(), authSaga()]);
 }
