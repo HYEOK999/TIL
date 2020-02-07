5b69e324fb007a22f9daaae94bdc843073ecc036
HYEOK999 <iasg2004@naver.com>
2020-02-07 23:41:05 +0900

    feat : Add the signinform containers

--- react/project/reactjs-books-review/src/containers/SigninFormContainer.js
+++ react/project/reactjs-books-review/src/containers/SigninFormContainer.js
@@ -1,14 +1,16 @@
 import { connect } from 'react-redux';
 import SigninForm from '../components/SigninForm';
-import { login } from '../redux/modules/auth';
+// import { login } from '../redux/modules/auth';
+import { signInSaga } from '../redux/modules/auth';
+
 export default connect(
   state => ({
     loading: state.auth.loading,
     error: state.auth.error,
   }),
   dispatch => ({
-    login: (email, password) => {
-      dispatch(login(email, password));
+    login: auth => {
+      dispatch(signInSaga(auth));
     },
   }),
 )(SigninForm);
