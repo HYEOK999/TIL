701b6ca4bceaaa76363ecd1959ee0affae2e7553
HYEOK999 <iasg2004@naver.com>
2020-02-07 23:41:21 +0900

    feat : Add the SinginForm container

--- react/project/reactjs-books-review/src/containers/NaviContainer.jsx
+++ react/project/reactjs-books-review/src/containers/NaviContainer.jsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import Navi from '../components/Navi';
-import { logout } from '../redux/modules/auth';
+// import { logout } from '../redux/modules/auth';
+import { signOutSaga } from '../redux/modules/auth';
 
 export default connect(
   state => ({ token: state.auth.token }),
   dispatch => ({
-    logout: token => {
-      dispatch(logout(token));
+    logout: () => {
+      dispatch(signOutSaga());
     },
   }),
 )(Navi);
