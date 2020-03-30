c199e8d59f680a4d9a8e42328558185c98dcea1d
HYEOK999 <iasg2004@naver.com>
2020-03-30 22:56:34 +0900

    feat : Studied th typescript

---
+++ typescript/dist/practice.js
@@ -0,0 +1,22 @@
+"use strict";
+// let count = 0;
+// count + 1;
+// // count = '갑자기 분위기 문자열';
+// const message: string = 'hello world';
+// const done: boolean = true;
+// const test1: number | undefined = undefined;
+// const numbers: number[] = [1, 2, 3];
+// const messages: any[] = ['hello', 'world'];
+// messages.push(1);
+// let mightBeUndefined: string | undefined = undefined;
+// let nullableNumber: number | null = null;
+// let color: 'red' | 'orange' | 'yellow' = 'red';
+// color = 'yellow';
+// // color = 'green'; // 에러 발생!
+// let a = (x : number, y:number) : number => {
+//   return x + y;
+// }
+// let b = function (x : number, y:number) : number {
+//   return x + y;
+// }
+// console.log(a);
